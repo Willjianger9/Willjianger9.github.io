1f53f9ed33f586325700549e968cd2d20c01c18f
William Jiang <w34jiang@uwaterloo.ca>
2025-02-02 13:16:08 -0800

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Willjianger9.github.io/',
+  base: process.env.NODE_ENV === 'production' ? '/Willjianger9.github.io/' : '/',
   plugins: [react()],
   server: {
     proxy: {
